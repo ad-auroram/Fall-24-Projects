e7e241758c3de91378553f79f02d6a780b6112be
rseeley <roxseeley@gmail.com>
2024-09-08 13:24:29 -0600

    drafted compose

--- WebApps/Assign1/server.py
+++ WebApps/Assign1/server.py
@@ -39,55 +39,73 @@ def makeHeaders(data):
     for i in range(len(data)):
         entry= data[i].replace("\r","")
         entries = entry.split(":",1)
-        print(entries[0])
-        headers.update({entries[0]:entries[1]})
+        headers[entries[0]]=entries[1]
     return headers
 
 #decides which endpoint to call
 #router
 def router(req):
-    middleware=[]
-    if req.uri == "/about": compose(about(req), middleware)
-    elif req.uri == "/experience": (experience(req), middleware)
-    elif req.uri == "/": (experience(req), middleware)
-    elif req.uri == "/projects": (projects(req), middleware)
-    elif req.uri == "/info": (info(req), middleware)
+    if req.uri == "/about": (about(req))
+    elif req.uri == "/experience": (experience(req))
+    elif req.uri == "/": (experience(req))
+    elif req.uri == "/projects": (projects(req))
+    elif req.uri == "/info": (info(req))
     else: print("URI not found")
 #middleware
 def compose(end_result_function, middleware_factory_list):
 # your implementation here, remove the `pass` when you are done
+    def composed(request):
+        for middleware in reversed(middleware_factory_list):
+            request=middleware(request)
+        response = end_result_function(request)
+        return response
+    return composed
+
+def logging(data):
+    if isinstance(data, Request):
+        print(data.method + " " + data.uri)
+    elif isinstance(data, Response):
+        print(data.uri + " "+ data.code + " "+ data.reason)
+
+def findStatic(data):
+    #if static file is requested, send in a response
     pass
+    
+def makeGoodHeaders(req, text):
+    headers={"Server:" : "Cool guy server", 
+             "Date:": datetime.datetime.now(), 
+             "Connection:" : "close", 
+             "Cache-Control:": "max-age=200"
+            }  
+    return headers
 
+def error():
+    print("uh oh")
 
-#endpoints (correspond to templates)
 
 # called by the router when the URI is /about
 def about(req):
-    file = open("/templates/about.html")
+    file = open("templates/about.html")
     response_text = file.read()
-    response = Response("X","Y","Z","A", response_text)
-    return response
+    return response_text
 
 # called by the router when the URI is /experience
 def experience(req):
-    file = open("/templates/experience.html")
+    file = open("templates/experience.html")
     response_text = file.read()
-    response = Response("X","Y","Z","A", response_text)
-    return response
+    return response_text
 
 # called by the router when the URI is /
 def index(req):
     file = open("templates/index.html")
     response_text = file.read()
-    response = Response("X","Y","Z","A", response_text)
     return response_text
 
 # called by the router when the URI is /projects
 def projects(req):
-    file = open("/templates/projects.html")
+    file = open("templates/projects.html")
     response_text = file.read()
-    response = Response("X","Y","Z","A", response_text)
-    return response
+    return response_text
 
 # called by the router when the URI is /info
 def info(req):
@@ -112,10 +130,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             print(data)
             dataList=data.strip("\r").split("\n")
             requestText=dataList[0]
-            requestHeaders = makeHeaders(dataList[1:])
-            request= Request(requestText.split[0], requestText.split[1], requestText.split[2], requestText, requestHeaders)
-            print(request)
+            requestHeaders = makeHeaders(dataList[1:len(dataList)-2])
+            requestInfo=requestText.split(" ")
+            request= Request(requestInfo[0], requestInfo[1], requestInfo[2], requestText, requestHeaders)
+            #testing
+            print(request.uri)
+            print(request.text)
             #TODO: parse the request, send through middleware and encode the response
+            middleware_list=[logging, findStatic]
             text=index(dataList[1])
-            res = "HTTP/1.1 200 Ok\nConnection: close\n\n" + text
+            res=compose(router(request),middleware_list)
+            #res = "HTTP/1.1 200 Ok\nConnection: close\n\n" + text
             connection.send(bytes(res, "UTF-8"))
