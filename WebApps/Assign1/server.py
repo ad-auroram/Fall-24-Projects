ea4bb79b921dbf699331493d3f82e021bcae9212
rseeley <roxseeley@gmail.com>
2024-09-09 17:49:07 -0600

    fixing logging, formatting, and typos

--- WebApps/Assign1/server.py
+++ WebApps/Assign1/server.py
@@ -39,18 +39,12 @@ class Response:
         
         response = (
             f"{self.version} {self.code} {self.reason}\r\n"
-            f"{headers}\r\n"
+            f"{headers}\r\n\r\n"
             f"{self.text}"
         )
         return response
 
-def makeRequestHeaders(data):
-    headers = {}
-    for i in range(len(data)):
-        entry= data[i].replace("\r","")
-        entries = entry.split(":",1)
-        headers[entries[0]]=entries[1]
-    return headers
+
 
 #decides which endpoint to call
 #router
@@ -67,19 +61,17 @@ def router(req):
         return info(req)
     else:
         headers = {"Content-Type": "text/plain", "Connection": "close"}
-        return Response("HTTP/1.1", 404, "Not Found", headers, "Page not found")
+        return Response("HTTP/1.1", "404", "Not Found", headers, "Page not found")
 
 #middleware
 def compose(end_result_function, middleware_factory_list):
     def composed_function(request):
-        for middleware in reversed(middleware_factory_list):
-            request = middleware(request)
-            if isinstance(request, Response):
-                break
-        if not isinstance(request, Response):
-            response = end_result_function(request)
-        else:
-            response = request
+        for middleware in (middleware_factory_list):
+            response = middleware(request)
+            if isinstance(response, Response):
+                return response
+        response = end_result_function(response)
+        logging(response)
         return response
     return composed_function
 
@@ -88,7 +80,7 @@ def logging(data):
         print("Request:" + data.method + " " + data.uri)
         return data
     elif isinstance(data, Response):
-        print("Response:"+ data.uri + " "+ data.code + " "+ data.reason)
+        print("Response:"+ data.version + " "+ data.code + " "+ data.reason)
         return data
 
 def findStatic(data):
@@ -108,26 +100,37 @@ def findStatic(data):
                 "Content-Type": content_type,
                 "Content-Length": str(len(response_text)),
             }
-            return Response("HTTP/1.1", 200, "OK", headers, response_text)
+            response = Response("HTTP/1.1", "200", "OK", headers, response_text)
+            logging(response)
+            return response
         
         except FileNotFoundError:
             headers = {
                 "Content-Type": "text/plain",
                 "Connection": "close"
             }
-            return Response("HTTP/1.1", 404, "Not Found", headers, "File not found")
+            response =Response("HTTP/1.1", "404", "Not Found", headers, "File not found")
+            logging(response)
+            return response
     return data
     
+#creates a disctionary of headers the responses have in common
 def makeGoodHeaders(req):
-    headers={"Server:" : "Cool guy server", 
-             "Date:": datetime.datetime.now(), 
-             "Connection:" : "close", 
-             "Cache-Control:": "max-age=200"
+    headers={"Server" : "Cool guy server", 
+             "Date": datetime.datetime.now(), 
+             "Connection" : "close", 
+             "Cache-Control": "max-age=200"
             }  
     return headers
-def error():
-    print("uh oh")
 
+#makes a dictionary of headers from the request
+def makeRequestHeaders(data):
+    headers = {}
+    for i in range(len(data)):
+        entry= data[i].replace("\r","")
+        entries = entry.split(":",1)
+        headers[entries[0]]=entries[1]
+    return headers
 
 #endpoints
 # called by the router when the URI is /about
@@ -144,7 +147,6 @@ def experience(req):
 def index(req):
     file = open("templates/index.html")
     response_text = file.read()
-    print("hello there")
     return makeResponse(req, "200", response_text)
 # called by the router when the URI is /projects
 def projects(req):
@@ -154,7 +156,6 @@ def projects(req):
 # called by the router when the URI is /info
 def info(req):
     # returns an HTTP redirect response that redirects the client to `/about`. 
-    # You should use 301 for the static code. Don't forget to set the `Location` header to tell the browser where to go.
     return makeResponse(req, "301", "placeholder")
 
 
@@ -181,21 +182,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 connection.close()
                 continue
             data = data.decode()
-            print(data)
             dataList=data.strip("\r").split("\n")
             requestText=dataList[0]
             requestHeaders = makeRequestHeaders(dataList[1:len(dataList)-2])
             requestInfo=requestText.split(" ")
             request= Request(requestInfo[0], requestInfo[1], requestInfo[2], requestText, requestHeaders)
             #testing
-            print(request.uri)
-            print(request.text)
+            
             #TODO: parse the request, send through middleware and encode the response
-            middleware_list = [findStatic, logging]
+            middleware_list = [logging, findStatic]
             composed_function = compose(router, middleware_list)
             response = composed_function(request)
             
             # Convert response to string and send
             res = response.to_string()
+            
             connection.sendall(res.encode("UTF-8"))
-            #res = "HTTP/1.1 200 Ok\nConnection: close\n\n" + text
