b31fdbea334be15289af6a7092b2375a12bede6c
rseeley <roxseeley@gmail.com>
2024-09-09 18:01:21 -0600

    Finishing touches

--- WebApps/Assign1/server.py
+++ WebApps/Assign1/server.py
@@ -1,6 +1,3 @@
-
-#run server
-#decode requests
 import socket
 import datetime
 
@@ -44,10 +41,7 @@ class Response:
         )
         return response
 
-
-
-#decides which endpoint to call
-#router
+#router-decides which endpoint to call
 def router(req):
     if req.uri == "/about":
         return about(req)
@@ -60,8 +54,8 @@ def router(req):
     elif req.uri == "/info":
         return info(req)
     else:
-        headers = {"Content-Type": "text/plain", "Connection": "close"}
-        return Response("HTTP/1.1", "404", "Not Found", headers, "Page not found")
+        headers = {"Content-Type": "text/html", "Connection": "close"}
+        return Response("HTTP/1.1", "404", "Not Found", headers, "<h1>404</h1><span>Page not found</span")
 
 #middleware
 def compose(end_result_function, middleware_factory_list):
@@ -82,7 +76,7 @@ def logging(data):
     elif isinstance(data, Response):
         print("Response:"+ data.version + " "+ data.code + " "+ data.reason)
         return data
-
+    
 def findStatic(data):
     if data.uri.startswith("/static/"):
         file_path = data.uri[len("/static/"):]
@@ -114,7 +108,8 @@ def findStatic(data):
             return response
     return data
     
-#creates a disctionary of headers the responses have in common
+
+#creates a dictionary of headers the responses have in common
 def makeGoodHeaders(req):
     headers={"Server" : "Cool guy server", 
              "Date": datetime.datetime.now(), 
@@ -132,39 +127,39 @@ def makeRequestHeaders(data):
         headers[entries[0]]=entries[1]
     return headers
 
+
 #endpoints
-# called by the router when the URI is /about
 def about(req):
     file = open("templates/about.html")
     response_text = file.read()
     return makeResponse(req, "200", response_text)
-# called by the router when the URI is /experience
+
 def experience(req):
     file = open("templates/experience.html")
     response_text = file.read()
     return makeResponse(req, "200", response_text)
-# called by the router when the URI is /
+
 def index(req):
     file = open("templates/index.html")
     response_text = file.read()
     return makeResponse(req, "200", response_text)
-# called by the router when the URI is /projects
+
 def projects(req):
     file = open("templates/projects.html")
     response_text = file.read()
     return makeResponse(req, "200", response_text)
-# called by the router when the URI is /info
+
 def info(req):
     # returns an HTTP redirect response that redirects the client to `/about`. 
     return makeResponse(req, "301", "placeholder")
 
-
+#creates the response object
 def makeResponse(req, code, text):
     headers = makeGoodHeaders(req)
     if code == "200":
         response = Response("HTTP/1.1", code, "OK", headers, text)
     elif code == "301":
-        headers = {"Location": "/about"}  # Redirects to "/about"
+        headers = {"Location": "/about"}
         response = Response("HTTP/1.1", code, "Moved Permanently", headers, "")
     return response
 
@@ -187,14 +182,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             requestHeaders = makeRequestHeaders(dataList[1:len(dataList)-2])
             requestInfo=requestText.split(" ")
             request= Request(requestInfo[0], requestInfo[1], requestInfo[2], requestText, requestHeaders)
-            #testing
-            
-            #TODO: parse the request, send through middleware and encode the response
             middleware_list = [logging, findStatic]
             composed_function = compose(router, middleware_list)
             response = composed_function(request)
-            
-            # Convert response to string and send
             res = response.to_string()
-            
             connection.sendall(res.encode("UTF-8"))
