d9efecf2381b95dd2b1af50992bb35aec9684c4f
rseeley <roxseeley@gmail.com>
2024-09-14 18:21:54 -0600

    coding along

--- WebApps/django_demo/myproject/myapp/views.py
+++ WebApps/django_demo/myproject/myapp/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, "django-demo/index.html")
+    return render(request, "django_demo/index.html")
+
+def passwords(request):
+    return HttpResponse("TEST")
