876ac2f603941a0baae901a25e7141e6f60cb6f7
rseeley <roxseeley@gmail.com>
2024-09-21 13:14:07 -0600

    Server runs

--- WebApps/Assign2/Tracker/Tracker/settings.py
+++ WebApps/Assign2/Tracker/Tracker/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.staticfiles',
-    'tracker-app.apps.MyappConfig'
+    'trackerApp.apps.TrackerConfig'
 ]
 
 MIDDLEWARE = [
