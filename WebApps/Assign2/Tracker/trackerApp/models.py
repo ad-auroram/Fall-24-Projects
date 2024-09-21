1074b2aa65a3f2c739a0b09097b2a3f55b3d4d66
rseeley <roxseeley@gmail.com>
2024-09-21 13:53:46 -0600

    Created models and templates

--- WebApps/Assign2/Tracker/trackerApp/models.py
+++ WebApps/Assign2/Tracker/trackerApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import timedelta
 
 # Create your models here.
+class Activity(models.Model):
+    id = models.IntegerField
+    name = models.CharField
+
+class TimeLog(models.Model):
+    id = models.IntegerField
+    start_time = models.DateTimeField
+    end_time = models.DateTimeField
+    activity =models.ForeignKey(Activity, on_delete=models.CASCADE)
