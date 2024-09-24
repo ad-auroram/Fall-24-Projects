385b5866b4e37b08f161718f70debd7387816213
rseeley <roxseeley@gmail.com>
2024-09-24 17:30:42 -0600

    figuring out more view things, starting styling

--- WebApps/Assign2/Tracker/trackerApp/urls.py
+++ WebApps/Assign2/Tracker/trackerApp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    
+    path("/new_activity", views.newActivity, name="index"),
+    path("/activity/{placeholder}", views.activity, name="index"),
+    path("/activity/{placeholder}/new_timelog", views.newTimelog, name="index")
 ]
--- WebApps/Assign2/Tracker/trackerApp/views.py
+++ WebApps/Assign2/Tracker/trackerApp/views.py
@@ -4,17 +4,15 @@ from django.http import HttpResponse
 
 def index(request):
     activities = []
-
+    #TODO: add all activities to a list
     return render(request, "Tracker/index.html")
 
 def newActivity(request):
-
     return render(request, "Tracker/new_activity.html")
 
 def activity(request):
-
+    #activity = get the activity by requested id
     return render(request, "Tracker/activity.html")
 
 def newTimelog(request):
-
     return render(request, "Tracker/new_time.html")
