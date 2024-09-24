b7838bd4807a04da2f16bff1c748ae39e9e41d9e
rseeley <roxseeley@gmail.com>
2024-09-24 17:10:39 -0600

    listed views and created another template

--- WebApps/Assign2/Tracker/trackerApp/views.py
+++ WebApps/Assign2/Tracker/trackerApp/views.py
@@ -3,4 +3,18 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
+    activities = []
+
     return render(request, "Tracker/index.html")
+
+def newActivity(request):
+
+    return render(request, "Tracker/new_activity.html")
+
+def activity(request):
+
+    return render(request, "Tracker/activity.html")
+
+def newTimelog(request):
+
+    return render(request, "Tracker/new_time.html")
