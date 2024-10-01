b16b563b9c9afff9e59c59550597044470285b5b
Rox <roxseeley@gmail.com>
2024-10-01 17:47:51 -0600

    Continually asks for input as long as no exit command given

--- Operating Systems/Assign3/src/shell.java
+++ Operating Systems/Assign3/src/shell.java
@@ -1,16 +1,24 @@
+import java.util.Objects;
 import java.util.Scanner;
 public class shell {
 
     public static void mkdir(String directory, String arg){
-        System.out.println("Currecnt directory: " +directory);
+        System.out.println("Current directory: " +directory);
         System.out.println(arg);
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.printf("[%s]: ", System.getProperty("user.dir"));
-        String currentDir = System.getProperty("user.dir");
-        String arguments = input.nextLine();
-        mkdir(currentDir, arguments);
+        boolean running = true;
+        while (running) {
+            System.out.printf("[%s]: ", System.getProperty("user.dir"));
+            String currentDir = System.getProperty("user.dir");
+            String arguments = input.nextLine();
+            if (Objects.equals(arguments, "exit")) {
+                running = false;
+            }else{
+                mkdir(currentDir, arguments);
+            }
+        }
     }
 }
