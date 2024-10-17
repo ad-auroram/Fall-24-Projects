4166eae54993a716e8a76f9ff00ac60883b4c5ef
rseeley <roxseeley@gmail.com>
2024-10-16 18:57:21 -0600

    added urls

--- WebApps/Assign3/Destinations/destinationsApp/urls.py
+++ WebApps/Assign3/Destinations/destinationsApp/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # put your paths here
+    path("", views.index, name="index"),
+    path("users/new", views.newUser, name="newUser"),
+    path("sessions/new", views.newSession, name="newSession"),
+    path("users", views.users, name="users"),
+    path("sessions", views.sessions, name="sessions"),
+    path("sessions/destroy", views.index, name="delete"),
+    path("destinations", views.index, name="destinations"),
+    path("destinations/new", views.index, name="newDestination"),
+    path("destinations/<int:id>", views.index, name="destinationId"),
+    path("destinations/<int: id>/destroy", views.index, name="index"),
+
 ]
