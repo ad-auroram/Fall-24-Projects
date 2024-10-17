59d72e03be0f02dbf1e61a5ffe9f44e55e0a7ba9
rseeley <roxseeley@gmail.com>
2024-10-16 22:16:22 -0600

    working on views

--- WebApps/Assign3/Destinations/destinationsApp/urls.py
+++ WebApps/Assign3/Destinations/destinationsApp/urls.py
@@ -5,12 +5,10 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("users/new", views.newUser, name="newUser"),
     path("sessions/new", views.newSession, name="newSession"),
-    path("users", views.users, name="users"),
-    path("sessions", views.sessions, name="sessions"),
-    path("sessions/destroy", views.index, name="delete"),
-    path("destinations", views.index, name="destinations"),
-    path("destinations/new", views.index, name="newDestination"),
-    path("destinations/<int:id>", views.index, name="destinationId"),
-    path("destinations/<int: id>/destroy", views.index, name="index"),
+    #path("sessions/destroy", views.deleteSession, name="delete"),
+    path("destinations", views.destinations, name="destinations"),
+    path("destinations/new", views.newDestination, name="newDestination"),
+    path("destinations/<int:id>", views.editEntry, name="edit"),
+    #path("destinations/<int: id>/destroy", views.deleteEntry, name="deleteEntry"),
 
 ]
--- WebApps/Assign3/Destinations/destinationsApp/views.py
+++ WebApps/Assign3/Destinations/destinationsApp/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import User, Destination, Session
 # Create your views here.
+def index(request):
+    #activities = Activity.objects.all()
+    return render(request, "Destinations/index.html")
+
+
+def newUser(request):
+    if request.method == "POST":
+        params = request.POST
+        user = User(
+            name = params.get("name"),
+            email = params.get("email"),
+            password_hash = params.get("password"), #TODO: FIGURE OUT PASSWORD HASHING
+        )
+        user.save()
+        #logged in now, make cookie for that
+        return redirect("destinations")
+    return render(request, "Destinations/new_user.html")
+
+def newSession(request):
+    if request.method == "POST":
+        params = request.POST
+        #check if user in table
+        #check if password is legit
+        #make session cookie, we're logged in now
+        return redirect("destinations")
+    return render(request, "Destinations/new_session.html")
+
+def destinations(request):
+    return render(request, "Destinations/destinations.html")
+
+def newDestination(request):
+    return render(request, "Destinations/new_destination")
+
+def editEntry(request):
+    return render(request, "Destination/edit_destination")
