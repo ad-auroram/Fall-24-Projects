e4771776298b3990f2ce479955b9d3a1d2868038
rseeley <roxseeley@gmail.com>
2024-10-16 22:27:45 -0600

    planned out remaining views

--- WebApps/Assign3/Destinations/destinationsApp/views.py
+++ WebApps/Assign3/Destinations/destinationsApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .models import User, Destination, Session
 # Create your views here.
 def index(request):
-    #activities = Activity.objects.all()
+    #make a list of 5 recents and return with render
     return render(request, "Destinations/index.html")
 
 
@@ -29,10 +29,24 @@ def newSession(request):
     return render(request, "Destinations/new_session.html")
 
 def destinations(request):
+    #make a list of destination that belong to that user
+    #return it with the render and add a loop to the page
     return render(request, "Destinations/destinations.html")
 
 def newDestination(request):
+    if request.method == "POST":
+        params = request.POST
+        destination = Destination(
+            name= params.get("destination"),
+            review = params.get("review"),
+            rating = params.get("rating"),
+            share_publically = params.get("public"),
+            #get the user from the form
+        )
+        destination.save()
+        return redirect("destinations")
     return render(request, "Destinations/new_destination")
 
 def editEntry(request):
+    #figure out how to autofill the form
     return render(request, "Destination/edit_destination")
