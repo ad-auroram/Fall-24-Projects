ff733b3d7f0313b718299be0cebdf5a32b96651c
Rox <roxseeley@gmail.com>
2024-10-22 19:55:05 -0600

    Got rid of test file

--- Operating Systems/Assign4/src/test.java
+++
@@ -1,8 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        Bpp bpp = new Bpp();
-        long place =4;
-        long val = bpp.getDecimal(place);
-        System.out.println(val);
-    }
-}
