0e0bebed508f5465458ef46686e29a93fb164f43
Rox <roxseeley@gmail.com>
2024-11-01 12:58:29 -0600

    Starter code

---
+++ Operating Systems/Assn5/Assn5-Starter/SchedulerFCFS.java
@@ -0,0 +1,2 @@
+//first come first serve
+//whatever's put on the queue first is the one that gets workd on first
---
+++ Operating Systems/Assn5/Assn5-Starter/SchedulerSJF.java
@@ -0,0 +1,2 @@
+//shortest job first
+//whichever has the shortst time to complete goes first
