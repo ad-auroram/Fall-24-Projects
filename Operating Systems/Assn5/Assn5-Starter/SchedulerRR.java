61e6502c40683b0554b823c82662b7ecd37ce5ab
Rox <roxseeley@gmail.com>
2024-11-05 12:55:26 -0700

    started RR with FCFS code, modified to track time as well.

--- Operating Systems/Assn5/Assn5-Starter/SchedulerRR.java
+++ Operating Systems/Assn5/Assn5-Starter/SchedulerRR.java
@@ -1,12 +1,64 @@
 //round robin
 public class SchedulerRR extends Scheduler{
-    public SchedulerRR(Platform platform, int time) {
-        super();
-    }
+    public class SchedulerFCFS extends Scheduler {
+        private Queue<Process> readyQueue; // Queue to store ready processes
+        private Platform platform;
+        private int time;
+        private int savedTime;
+
+        public SchedulerFCFS(Platform platform, int time) {
+            this.platform = platform;
+            this.readyQueue = new LinkedList<>();
+            this.time = time;
+            this.savedTime=time
+        }
+        //starting with fcfs code, modify to take time into account
     public void notifyNewProcess(Process process){
-        System.out.println("new Process");
+        readyQueue.add(process);
     }
-    public Process update(Process process, int cpu){
-        return process;
+    public Process update(Process process, int cpu) {
+        if (process == null) {
+            if (!readyQueue.isEmpty()){
+                time = savedTime;
+                Process next= readyQueue.poll();
+                platform.log("CPU "+cpu+" > Scheduled "+ next.getName());
+                time--
+                return next;
+            } else {
+                return null;
+            }
+        }
+        if (time=0){
+            Process next= readyQueue.poll();
+            platform.log("CPU "+cpu+" > Time quantum complete for process "+ next.getName());
+            platform.log("CPU "+cpu+" > Scheduled "+ next.getName());
+            contextSwitches++;
+            return next;
+        }
+
+        }
+        if (!process.isBurstComplete()) {
+            time --;
+            return process;
+        } else {
+            if (!process.isExecutionComplete()){
+                platform.log("CPU "+cpu+" > Process "+ process.getName()+" burst complete");
+                readyQueue.add(process);
+            }
+            else{
+                platform.log("CPU "+cpu+" > Process "+ process.getName()+" burst complete");
+                platform.log("CPU "+cpu+" > Process "+ process.getName()+" execution complete");
+            }
+            // go to next process if current is done
+            if (!readyQueue.isEmpty()) {
+                Process nextProcess = readyQueue.poll();
+                platform.log("CPU "+cpu+" > Scheduled "+ nextProcess.getName());
+                contextSwitches++;
+                time --;
+                return nextProcess;
+            } else {
+                return null;
+            }
+        }
     }
 }
