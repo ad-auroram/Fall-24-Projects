a94ea37ddb98e61ce1229b9d4c9c17cae4b61f97
Rox <roxseeley@gmail.com>
2024-11-04 23:10:53 -0700

    planning with FSCS as skeleton

--- Operating Systems/Assn5/Assn5-Starter/SchedulerSJF.java
+++ Operating Systems/Assn5/Assn5-Starter/SchedulerSJF.java
@@ -1,14 +1,60 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 //shortest job first
 //whichever has the shortst time to complete goes first
 public class SchedulerSJF extends Scheduler{
+    private Queue<Process> readyQueue; // Queue to store ready processes
+    private Platform platform;
+
     public SchedulerSJF(Platform platform) {
-        super();
+        this.platform = platform;
+        this.readyQueue = new LinkedList<>();
     }
 
-    public void notifyNewProcess(Process process){
-        System.out.println("new Process");
+    public void notifyNewProcess(Process process) {
+        readyQueue = sort(process);
+
     }
-    public Process update(Process process, int cpu){
-        return process;
+    private LinkedList<Process> sort(Process process) {
+        //make and arraylist, sort it, return as linked list
+    }
+
+    /**
+     * Update method for scheduling a process on a CPU.
+     * FCFS schedules the next process in the ready queue based on arrival time.
+     */
+    public Process update(Process process, int cpu) {
+        if (process == null) {
+            if (!readyQueue.isEmpty()){
+                Process next= readyQueue.poll();
+                platform.log("CPU "+cpu+" > Scheduled "+ next.getName());
+                return next;
+            } else {
+                return null;
+            }
+        }
+
+        if (!process.isBurstComplete()) {
+            return process;
+        } else {
+            if (!process.isExecutionComplete()){
+                platform.log("CPU "+cpu+" > Process "+ process.getName()+" burst complete");
+                readyQueue.add(process);
+            }
+            else{
+                platform.log("CPU "+cpu+" > Process "+ process.getName()+" burst complete");
+                platform.log("CPU "+cpu+" > Process "+ process.getName()+" execution complete");
+            }
+            // go to next process if current is done
+            if (!readyQueue.isEmpty()) {
+                Process nextProcess = readyQueue.poll();
+                platform.log("CPU "+cpu+" > Scheduled "+ nextProcess.getName());
+                contextSwitches++;
+                return nextProcess;
+            } else {
+                return null;
+            }
+        }
     }
 }
