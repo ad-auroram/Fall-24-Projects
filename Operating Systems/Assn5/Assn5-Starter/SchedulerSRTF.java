f6429f3eeaed691ae1c0dcbee58e4d8e11e6f99d
Rox <roxseeley@gmail.com>
2024-11-05 18:55:25 -0700

    srtf doesn't swap the current process if there's a faster one, but otherwise works

--- Operating Systems/Assn5/Assn5-Starter/SchedulerSRTF.java
+++ Operating Systems/Assn5/Assn5-Starter/SchedulerSRTF.java
@@ -1,13 +1,59 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 //shortest remaining time first
 public class SchedulerSRTF extends Scheduler{
+    private PriorityQueue<Process> readyQueue; // Queue to store ready processes
+    private Platform platform;
+
     public SchedulerSRTF(Platform platform) {
-        super();
+        this.platform = platform;
+        readyQueue = new PriorityQueue<>(Comparator.comparingInt(p -> p.getTotalTime() - p.getElapsedTotal()));
     }
 
-    public void notifyNewProcess(Process process){
-        System.out.println("new Process");
+    public void notifyNewProcess(Process process) {
+        readyQueue.add(process);
+
     }
-    public Process update(Process process, int cpu){
-        return process;
+
+    /**
+     * Update method for scheduling a process on a CPU.
+     * FCFS schedules the next process in the ready queue based on arrival time.
+     */
+    public Process update(Process process, int cpu) {
+        if (process == null) {
+            if (!readyQueue.isEmpty()){
+                Process next= readyQueue.poll();
+                platform.log("CPU "+cpu+" > Scheduled "+ next.getName());
+                contextSwitches++;
+                return next;
+            } else {
+                return null;
+            }
+        }
+
+        if (!process.isBurstComplete()) {
+            return process;
+        } else {
+            if (!process.isExecutionComplete()){
+                platform.log("CPU "+cpu+" > Process "+ process.getName()+" burst complete");
+                readyQueue.add(process);
+                contextSwitches++;
+            }
+            else{
+                platform.log("CPU "+cpu+" > Process "+ process.getName()+" burst complete");
+                platform.log("CPU "+cpu+" > Process "+ process.getName()+" execution complete");
+                contextSwitches++;
+            }
+            // go to next process if current is done
+            if (!readyQueue.isEmpty()) {
+                Process nextProcess = readyQueue.poll();
+                platform.log("CPU "+cpu+" > Scheduled "+ nextProcess.getName());
+                contextSwitches++;
+                return nextProcess;
+            } else {
+                return null;
+            }
+        }
     }
 }
