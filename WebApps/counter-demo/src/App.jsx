79aff7f0396bdd546f6f6e5fac6fcd5fad423855
rseeley <roxseeley@gmail.com>
2024-11-06 18:31:52 -0700

    counter project

---
+++ WebApps/counter-demo/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+       
+      </div>
+      <h1>Counting</h1>
+      <div className='count'>{count}</div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          Increase +1
+        </button>
+        <button onClick={() => setCount((count) => count - 1)}>
+          Decrease -1
+        </button>
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 5)}>
+          Increase +5
+        </button>
+        <button onClick={() => setCount((count) => count - 5)}>
+          Decrease -5
+        </button>
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count * 2)}>
+          Multiply x2
+        </button>
+        <button onClick={() => setCount((count) => count / 2)}>
+          Divide /2
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
